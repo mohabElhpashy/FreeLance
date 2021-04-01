d0501a3a1c7ffe7ce783f50feef7765cf6092c27
mohabelhbashy <mohab.hafez332@gmail.com>
2021-04-01 16:23:07 +0200

    add new feeett

--- src/hooks/HomeRouter.js
+++ src/hooks/HomeRouter.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 function Home() {
   const GoTo = () => {
-    window.location = "http://localhost:3000/Home";
+    window.location = "/Home";
   };
 
   return [GoTo];
